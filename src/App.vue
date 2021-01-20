cbf1dabe8785ed5f014205651e755fab792c3c39
Martin Lindström <martin@osynlig.se>
2021-01-21 00:57:05 +0100

    Messing with routing and styling stuff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Social.gg logo" src="./assets/logo.svg">
+    <Start msg="Play social games at home with your friends."/>
+    <Game msg="Song battle"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Start from './components/Start.vue'
+import Game from './components/Game.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Start,
+    Game
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #2F383E;
+}
+
+h1 {
+  font-weight: bold;
+  font-size: 28px;
+  color: #FFEF65;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Inter, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #C7C7C7;
   margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
+
+const NotFound = { template: '<p>Page not found</p>' }
+const Start = App
+const Game = { template: '<p>Game page</p>' }
+
+const routes = {
+  '/': Start,
+  '/game': Game
+}
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
+
+// new Vue({
+//   el: '#app',
+//   data: {
+//     currentRoute: window.location.pathname
+//   },
+//   computed: {
+//     ViewComponent () {
+//       return routes[this.currentRoute] || NotFound
+//     }
+//   },
+//   render: h => h(App),
+// }).$mount('#app')
