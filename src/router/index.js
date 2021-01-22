1c59787b50aa5b9c2ad9d2178820287e964d399e
Martin Lindström <martin@osynlig.se>
2021-01-22 23:21:09 +0100

    Added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+
 import Home from "../views/Home.vue";
 import Game from "../views/Game.vue";
+import NotFound from "../views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -23,7 +25,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "*",
+    name: "Not found",
+    component: NotFound
   }
 ];
 
