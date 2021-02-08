b9054b50265da10577674f53c07f7614a90e58c2
Martin Lindström <martin@osynlig.se>
2021-02-09 00:33:56 +0100

    Header icons and z-index fix for overlays

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header>
-      <a class="left" @click="$router.go(-1)">Back</a>
-      <router-link to="/" class="logo"><img class="logo" alt="Social.gg logo" width="34" height="34" src="../assets/logo.svg" /></router-link>
-      <a class="right" v-on:click="$parent.showGameList = !$parent.showGameList">All games</a>
+      <a class="left" @click="$router.go(-1)"><img src="@/assets/icons/arrow-left.svg" width="9" height="17" alt="Back arrow icon">Back</a>
+      <router-link to="/" class="logo"><img class="logo" alt="Social.gg logo" width="34" height="34" src="@/assets/logo.svg" /></router-link>
+      <a class="right" v-on:click="$parent.showGameList = !$parent.showGameList"><img src="@/assets/icons/all-games.svg" width="16" height="17" alt="All games icon">All games</a>
   </header>
 </template>
 
