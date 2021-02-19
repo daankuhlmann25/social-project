df56d5bf5f8da82347f08c9f21a6335ed11e0706
Martin Lindström <martin@osynlig.se>
2021-02-19 01:57:26 +0100

    Started working on logic and styling for cards on AddDeck

--- src/views/EditDeck.vue
+++ src/views/EditDeck.vue
@@ -1,31 +1,39 @@
 <template>
-    <div class="edit-deck">
-        <h5>{{$route.params.gameId}}</h5>
-        <h1>{{$route.name}}</h1>
+  <div class="edit-deck">
+    <h5>{{$route.params.gameId}}</h5>
+    <h1>{{$route.name}}</h1>
 
-        <form @submit.prevent="AddDeck">
-            <div class="field title">
-                <label for="name"><h2>Deck name:</h2></label>
-                <input type="text" name="deck_name" placeholder="Deck name" v-model="deck_name">
-            </div>
-            <div class="description">
-                <label for="description"><h2>Description:</h2></label>
-                <textarea name="description" id="description" cols="30" rows="10" placeholder="What is in the deck?" v-model="description"></textarea>
-            </div>
-<!--            <div v-for="(ing, index) in cards" :key="index">-->
-<!--                <label for="card"><h2>Card:</h2></label>-->
-<!--                <input type="text" name="card" v-model="cards[index]">-->
-<!--            </div>-->
-<!--            <div class="add-card">-->
-<!--                <label for="add-card"><h2>Add a card:</h2></label>-->
-<!--                <input type="text" name="add-card" @keydown.tab.prevent="addCard" v-model="cards[index]">-->
-<!--            </div>-->
-            <button type="submit">Save Deck</button>
-            <p v-if="feedback" class="errors">{{ feedback }}</p>
+    <!-- <form @submit.prevent="addDeck"> -->
+    <form>
+      <div class="field title">
+        <label for="deck_name"><h2>Deck name:</h2></label>
+        <input type="text" id="deck_name" name="deck_name" placeholder="Name your deck" v-model="deck_name">
+      </div>
+      <div class="description">
+        <label for="description"><h2>Description:</h2></label>
+        <textarea name="description" id="description" cols="30" rows="10" placeholder="What is in the deck?" v-model="description"></textarea>
+      </div>
+      <h2>Cards</h2>
+      <button type="button" :class="card_id === index ? 'card selected' : 'card'" @click="selectCard(index)" v-for="(card, index) in cards" :key="card[index]" :v-model="card[index]">{{ index }}</button>
+      <button type="button" class="add-card" name="add-card" @click="addCard"><img src="@/assets/icons/plus.svg" width="20" height="20" alt="Plus icon"></button>
+      <section class="edit-card">
+        <input type="hidden" name="card_id" id="card_id" v-model="card_id">
+        <label for="artist"><h3>Artist</h3></label>
+        <input type="text" name="artist" id="artist" placeholder="Artist name" v-model="artist" v-on:blur="saveCard">
+        <label for="song"><h3>Song</h3></label>
+        <input type="text" name="song" id="song" v-model="song" v-on:blur="saveCard" placeholder="Song name">
+        <label for="you_sing"><h3>You sing</h3></label>
+        <textarea name="you_sing" id="you_sing" rows="6" v-model="you_sing" v-on:blur="saveCard" placeholder="Lyrics"></textarea>
+        <label for="they_sing"><h3>They sing</h3></label>
+        <textarea name="they_sing" id="they_sing" rows="6" v-model="they_sing" placeholder="Lyrics"></textarea>
+        <button type="button" @click="removeCard()" class="delete-card"><img src="@/assets/icons/delete.svg" width="16" height="16" alt="Delete card" title="Delete this card"></button>
+      </section>
+      <button type="submit">Save Deck</button>
+      <p v-if="feedback" class="errors">{{ feedback }}</p>
 
-            <p>We would love you to share your fantastic deck with all our fellow players!</p>
-            <p>When you feel happy with your creation, please publish it.</p>
-        </form>
+      <p>We would love you to share your fantastic deck with all our fellow players!</p>
+      <p>When you feel happy with your creation, please publish it.</p>
+    </form>
 
 
 <!--                <h2>Name <span>0/40</span></h2>-->
@@ -36,12 +44,12 @@
 <!--                <h2>E-mail <span>Not public</span></h2>-->
 <!--                <input type="email" name="email" id="email">-->
 <!--                <h2>E-mail <span>Not public</span></h2>-->
-<!--                <textarea name="message" id="message" cols="30" rows="10"></textarea>-->
+<!--                <textarea name="message" id="message" rows="6"></textarea>-->
 <!--                <input type="submit" value="Save">-->
 <!--                <p>We would love you to share your fantastic deck with all our fellow players!</p>-->
 <!--                <p>When you feel happy with your creation, please publish it.</p>-->
 <!--                <button type="submit">Publish on social.gg</button>-->
-    </div>
+  </div>
 </template>
 
 <script>
@@ -54,15 +62,27 @@
         deckId: Number,
         another: null,
         id: null,
+        card_id: 0,
         deck_name: null,
         name: null,
         description: null,
-        cards: [],
+        cards: [
+            {
+              artist: "Artist 1", 
+              song: "Song 1",
+              you_sing: "You sing 1",
+              they_sing: "they sing 1",
+            },
+          ],
+        artist: "",
+        song: "",
+        you_sing: "",
+        they_sing: "",
         feedback: null,
       }
     },
     methods: {
-      AddDeck() {
+      addDeck() {
         console.log('Deck name = '+this.deck_name)
         console.log('Description = '+this.description)
 
@@ -84,16 +104,59 @@
           this.feedback = 'You must enter a deck name!'
         }
       },
-      AddCard() {
-        console.log('AddCard runned')
-        if (this.another) {
-          this.cards.push(this.another)
-          this.another = null
-          this.feedback = null
-        } else {
-          this.feedback = 'You must enter a value to add a card!'
+      addCard() {
+        console.log('addCard'+this.cards.length)
+        this.saveCard()
+        this.cards.push({
+            id: this.cards.length,
+            artist: "", 
+            song: "",
+            you_sing: "",
+            they_sing: "",
+          })
+        this.selectCard(this.cards.length-1)
+      },
+      removeCard() {
+        console.log('removeCard ' + this.card_id)
+        
+        if (this.cards.length === 1)
+          this.clearCard(0)
+
+        else {
+          console.log('- slice: '+this.card_id);
+          
+          this.cards.splice(this.card_id, 1)
+          this.selectCard(0, false)
         }
-      }
+      },
+      clearCard() {
+        console.log('clearCard ' + this.card_id)
+        
+        this.cards[this.card_id].artist     = this.artist     = ""
+        this.cards[this.card_id].song       = this.song       = ""
+        this.cards[this.card_id].you_sing   = this.you_sing   = ""
+        this.cards[this.card_id].they_sing  = this.they_sing  = ""
+      },
+      selectCard(cardId, save = true) {
+        console.log('selectCard('+cardId+")")
+        
+        if (save)
+          this.saveCard()
+        
+        this.card_id    = cardId
+        this.artist     = this.cards[cardId].artist
+        this.song       = this.cards[cardId].song
+        this.you_sing   = this.cards[cardId].you_sing
+        this.they_sing  = this.cards[cardId].they_sing
+      },
+      saveCard() {
+        console.log('saveCard ' + this.card_id)
+        
+        this.cards[this.card_id].artist     = this.artist
+        this.cards[this.card_id].song       = this.song
+        this.cards[this.card_id].you_sing   = this.you_sing
+        this.cards[this.card_id].they_sing  = this.they_sing
+      },
     }
   }
 </script>
