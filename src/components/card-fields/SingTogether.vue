db675912f13d4feb012cc5e88279fc9c8e50ef2a
Martin Lindström <martin@osynlig.se>
2021-03-06 15:33:01 +0100

    added trim to inputs

--- src/components/card-fields/SingTogether.vue
+++ src/components/card-fields/SingTogether.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <label for="artist"><h3>Artist</h3></label>
-    <input type="text" name="artist" id="artist" placeholder="Artist name" v-model="buffer.artist" @input="$emit('input', buffer)">
+    <input type="text" name="artist" id="artist" placeholder="Artist name" v-model.trim="buffer.artist" @input="$emit('input', buffer)">
     <label for="song"><h3>Song</h3></label>
-    <input type="text" name="song" id="song" placeholder="Song name" v-model="buffer.song" @input="$emit('input', buffer)">
+    <input type="text" name="song" id="song" placeholder="Song name" v-model.trim="buffer.song" @input="$emit('input', buffer)">
     <label for="youSing"><h3>You sing</h3></label>
-    <textarea name="youSing" id="youSing" rows="3" placeholder="Lyrics" v-model="buffer.youSing" @input="$emit('input', buffer)"></textarea>
+    <textarea name="youSing" id="youSing" rows="3" placeholder="Lyrics" v-model.trim="buffer.youSing" @input="$emit('input', buffer)"></textarea>
     <label for="theySing"><h3>They sing</h3></label>
-    <textarea name="theySing" id="theySing" rows="3" placeholder="Lyrics" v-model="buffer.theySing" @input="$emit('input', buffer)"></textarea>
+    <textarea name="theySing" id="theySing" rows="3" placeholder="Lyrics" v-model.trim="buffer.theySing" @input="$emit('input', buffer)"></textarea>
   </div>
 </template>
 
