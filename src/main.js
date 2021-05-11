085c61322a7327a9506c01c123fedb87d3f0b3a5
Martin Lindström <martin@osynlig.se>
2021-05-12 01:24:16 +0200

    NL, SV and EN all set up

--- src/main.js
+++ src/main.js
@@ -4,15 +4,19 @@ import store from './store'
 import router from "./router"
 import VueI18n from "vue-i18n";
 import en from "./translations/en.json"
+import sv from "./translations/sv.json"
+import nl from "./translations/nl.json"
 import 'fast-text-encoding';
 
 const messages = {
   en,
+  sv,
+  nl,
 }
 
 Vue.use(VueI18n);
 const i18n = new VueI18n({
-  locale: 'en',
+  locale: 'sv',
   messages
 })
 
