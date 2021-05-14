4996f6eef9ed668c2dd850b2c57236b42cdf9e29
Martin Lindström <martin@osynlig.se>
2021-05-14 11:24:44 +0200

    cleanup

--- src/main.js
+++ src/main.js
@@ -2,18 +2,18 @@ import Vue from "vue"
 import App from "./App.vue"
 import store from './store'
 import router from "./router"
-import VueI18n from "vue-i18n";
-import 'fast-text-encoding';
+import VueI18n from "vue-i18n"
+import 'fast-text-encoding'
 import { i18n } from 'vue-lang-router'
 
 
-Vue.use(VueI18n);
+Vue.use(VueI18n)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-i18n,
+  i18n,
   store,
   render: h => h(App)
 }).$mount("#app")
