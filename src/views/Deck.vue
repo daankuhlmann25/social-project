e97d4268393e8c9c86dcc99a19b79f19b27553d5
Martin Lindström <martin@osynlig.se>
2021-05-19 23:32:21 +0200

    localized-link didn't work, changed it back

--- src/views/Deck.vue
+++ src/views/Deck.vue
@@ -7,7 +7,7 @@
     </hgroup>
     <MartDown class="description">{{ description }}</MartDown>
     <div class="play-deck">
-      <localized-link to="play" append><img src="@/assets/icons/play.svg" width="30" height="30" :alt="$t('Play icon')"></localized-link>
+      <router-link to="play" append><img src="@/assets/icons/play.svg" width="30" height="30" :alt="$t('Play icon')"></router-link>
       <span>{{ $t('Play') }}</span>
     </div>
   </div>
